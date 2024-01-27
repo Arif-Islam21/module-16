18d64a4b02dd8cd8270a0da58aef85d6855a7015
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-27 16:19:40 +0600

    task 6 done

---
+++ task6.js
@@ -0,0 +1,11 @@
+const age = 72;
+const isStudent = false;
+if (age < 10) {
+  console.log("Free");
+} else if (isStudent) {
+  console.log("50% off");
+} else if (age >= 60) {
+  console.log("gets 15% discount");
+} else {
+  console.log("ticket fare is 800tk");
+}
