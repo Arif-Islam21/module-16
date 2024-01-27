ce5ec054d82cfd0948aafa16f76d774083f15f28
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-27 16:15:37 +0600

    task 5 done

---
+++ task5.js
@@ -0,0 +1,11 @@
+const num1 = 50;
+const num2 = 80;
+let result;
+// if (num1 > num2) {
+//   result = 2 * num1;
+// } else {
+//   result = num1 + num2;
+// }
+// console.log(result);
+num1 > num2 ? (result = 2 * num1) : (result = num1 + num2);
+console.log(result);
