2a88d9fbe295a4d4cb91d126f406700c2f9e5125
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-28 19:08:02 +0600

    light camera action

--- switch.js
+++ switch.js
@@ -1,26 +1,37 @@
-const dayOfWeek = 4;
-switch (dayOfWeek) {
-  case 1:
-    console.log("saturday");
+// const dayOfWeek = 4;
+// switch (dayOfWeek) {
+//   case 1:
+//     console.log("saturday");
+//     break;
+//   case 2:
+//     console.log("Sunday");
+//     break;
+//   case 3:
+//     console.log("Monday");
+//     break;
+//   case 4:
+//     console.log("tuesday");
+//     break;
+//   case 5:
+//     console.log("wednesday");
+//     break;
+//   case 6:
+//     console.log("thusday");
+//     break;
+//   case 7:
+//     console.log("friday");
+//     break;
+//   default:
+//     console.log("Invalid Input");
+// }
+
+const lightCondition = true;
+switch (true) {
+  case lightCondition:
+    console.log("light is on");
     break;
-  case 2:
-    console.log("Sunday");
-    break;
-  case 3:
-    console.log("Monday");
-    break;
-  case 4:
-    console.log("tuesday");
-    break;
-  case 5:
-    console.log("wednesday");
-    break;
-  case 6:
-    console.log("thusday");
-    break;
-  case 7:
-    console.log("friday");
-    break;
+
   default:
-    console.log("Invalid Input");
+    console.log("light is off");
+    break;
 }
