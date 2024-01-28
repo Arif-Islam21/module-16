5120633045dafbfa2a8a348e78885bdc12915c64
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-28 18:53:17 +0600

    learned basic switch

---
+++ switch.js
@@ -0,0 +1,26 @@
+const dayOfWeek = 4;
+switch (dayOfWeek) {
+  case 1:
+    console.log("saturday");
+    break;
+  case 2:
+    console.log("Sunday");
+    break;
+  case 3:
+    console.log("Monday");
+    break;
+  case 4:
+    console.log("tuesday");
+    break;
+  case 5:
+    console.log("wednesday");
+    break;
+  case 6:
+    console.log("thusday");
+    break;
+  case 7:
+    console.log("friday");
+    break;
+  default:
+    console.log("Invalid Input");
+}
