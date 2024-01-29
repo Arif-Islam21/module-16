de9eff014d7fe6423ef08701b7ee3e83e1da17a8
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-29 22:57:32 +0600

    summation operation done

---
+++ loop in js/range.js
@@ -0,0 +1,6 @@
+let number = 0;
+while (number <= 10) {
+  //!focus on the condition
+  console.log(number);
+  number++;
+}
---
+++ loop in js/sum.js
@@ -0,0 +1,8 @@
+let num = 1;
+let total = 0;
+while (num <= 10) {
+  console.log(num);
+  total += num;
+  console.log(total);
+  num++;
+}
