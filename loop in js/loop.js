708d5e3257d0f5525f36d96381183f72175bb9e9
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-29 22:11:46 +0600

    fruits added

--- loop in js/loop.js
+++ loop in js/loop.js
@@ -3,3 +3,8 @@ console.log(nums);
 for (const num of nums) {
   console.log(num);
 }
+
+const fruits = ["orange", "Apple", "Lemons", "Guava", "PineApple"];
+for (const fruit of fruits) {
+  console.log(fruit);
+}
