e0f396824c9471d608771d4a03267d436473f7a9
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-30 18:21:46 +0600

    learning for loop

---
+++ js-loop-tasks/for.js
@@ -0,0 +1,11 @@
+/**
+ * for ( ; ; ){
+ *  loop statement
+ * }
+ * first condition: loop variable declaration
+ * second condition: loop condition declaration
+ * third condition : loop variable change
+ */
+for (let num = 0; num < 5; num++) {
+  console.log(num);
+}
