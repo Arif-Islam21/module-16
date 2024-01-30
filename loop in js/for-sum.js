d5304c50b008f1fd8e2178e4d607f6a9a227f869
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-30 18:56:34 +0600

    solving problems with loops

---
+++ loop in js/for-sum.js
@@ -0,0 +1,15 @@
+// let sum = 0;
+// for (let i = 11; i < 20; i++) {
+//   sum = sum + i;
+// }
+// console.log("sum of numbers are ", sum);
+
+// for (let i = 10; i >= 0; i--) {
+//   console.log(i);
+// }
+
+let num = 20;
+while (num >= 0) {
+  console.log(num);
+  num--;
+}
