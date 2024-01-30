99c653e2c89ab79ea791f922f79a8e36fe5ddb28
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-30 21:33:37 +0600

    commit done for next 60 days

---
+++ js-loop-tasks/while/commit.js
@@ -0,0 +1,12 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+/*programming hero*/
+let num = 0;
+while (num <= 60) {
+  console.log("I will invest 6 hrs every day for next 60 days inshaallah");
+  num++;
+}
