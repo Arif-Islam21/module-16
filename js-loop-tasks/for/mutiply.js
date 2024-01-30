e05ae6a5132f4a768fa9b580471d85540db9abb4
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-30 22:09:20 +0600

    multiply table done

---
+++ js-loop-tasks/for/mutiply.js
@@ -0,0 +1,11 @@
+/***
+
+Generate a multiplication table for number 9
+
+ */
+const number = 9;
+for (let i = 0; i <= 10; i++) {
+  console.log(`${number} X ${i} = ${number * i}`);
+}
+
+/*programming hero*/
