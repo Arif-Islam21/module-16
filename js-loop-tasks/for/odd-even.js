1f8ec030e404ef208f02e296b7b886671b9ee73b
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-30 22:02:04 +0600

    odd even done with for loop

--- js-loop-tasks/for/odd-even.js
+++ js-loop-tasks/for/odd-even.js
@@ -6,7 +6,11 @@ Find all the odd numbers from 61 to 100.
 
  */
 
-
+for (let i = 61; i <= 100; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
 /***
 
 Subtask-2:
@@ -14,5 +18,9 @@ Subtask-2:
 Find all the even numbers from 78 to 98.
 
  */
-
+for (let i = 78; i <= 98; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
 /*programming hero*/
