5a61e016dc0d92c7958db4d095cd4b4b7127f37f
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-30 19:32:38 +0600

    learned break and continue

--- loop in js/break.js
+++ loop in js/break.js
@@ -1,6 +1,24 @@
-for (let i = 0; i < 15; i++) {
-  console.log(i);
-  if (i > 10) {
-    break;
+// for (let i = 0; i < 15; i++) {
+//   console.log(i);
+//   if (i > 10) {
+//     break;
+//   }
+// }
+
+// let nums = 54;
+// while (nums > 25) {
+//   console.log(nums);
+//   nums--;
+//   if (nums < 50) {
+//     break;
+//   }
+// }
+
+// CONTINUE ==> SKIP THIS ONE
+//BREAK ==> LAST OF THE LOOP
+for (let i = 1; i < 10; i++) {
+  if (i % 2 === 1) {
+    continue;
   }
+  console.log(i);
 }
