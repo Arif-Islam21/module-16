604e49e57306e73e85f7fa32199cbbcee149b1fe
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-30 18:32:27 +0600

    odd even number detected

--- loop in js/for.js
+++ loop in js/for.js
@@ -6,6 +6,14 @@
  * second condition: loop condition declaration
  * third condition : loop variable change
  */
-for (let num = 0; num < 5; num++) {
-  console.log(num);
+// for (let num = 0; num < 5; num++) {
+//   console.log(num);
+// }
+
+// ODD EVEN NUMBER DETECTION
+for (let i = 0; i < 10; i++) {
+  // console.log(i);
+  if (i % 2 === 0) {
+    console.log(i);
+  }
 }
