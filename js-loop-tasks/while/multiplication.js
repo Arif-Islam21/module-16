eb7391c4eb0ec55a9b3327e7dea7372d9f58a6b0
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-30 21:51:56 +0600

    multiplication table creation done

---
+++ js-loop-tasks/while/multiplication.js
@@ -0,0 +1,13 @@
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+
+let table = 5;
+let multipier = 0;
+while (multipier <= 10) {
+  console.log(`${table} X ${multipier} = ${table * multipier}`);
+  multipier++;
+}
+/*programming hero*/
