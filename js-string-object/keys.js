b2ee57e379a5a5c38328d3f896565e9545c433c5
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-31 23:52:17 +0600

    learned about basic nestet object

---
+++ js-string-object/keys.js
@@ -0,0 +1,19 @@
+// const computer = {
+//   brand: "China",
+//   Price: 20000,
+//   processor: "intel",
+//   hdd: "128gb",
+//   monitor: "chinise",
+// };
+// const keys = Object.keys(computer);
+// console.log(keys);
+
+const computer = {
+  brand: "China",
+  Price: 20000,
+  processor: "intel",
+  hdd: "128gb",
+  monitor: "chinise",
+};
+const values = Object.values(computer);
+console.log(values);
---
+++ js-string-object/nested.js
@@ -0,0 +1,13 @@
+const collage = {
+  name: "vge",
+  class: ["11", "12"],
+  events: ["science fair", "bijoy dibos", "21 february"],
+  unique: {
+    color: "blue",
+    result: {
+      gpa: 5,
+      marit: "top",
+    },
+  },
+};
+console.log(collage.unique.result.gpa);
