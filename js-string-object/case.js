2a75f68e27c34bc179ff553d38384480424955b1
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-31 09:58:43 +0600

    learned about case and trim

--- js-string-object/case.js
+++ js-string-object/case.js
@@ -1,5 +1,21 @@
 // uppercase and lower case matter in programmingn language
-const school = "Kurigram Politechnic Institute";
-console.log(school);
-console.log(school.toLowerCase());
-console.log(school.toUpperCase());
+// const school = "Kurigram Politechnic Institute";
+// console.log(school);
+// console.log(school.toLowerCase());
+// console.log(school.toUpperCase());
+
+// const book = "Chemistry";
+// const subject = "chemistry";
+// if (book.toLowerCase() === subject.toLowerCase()) {
+//   console.log("I am reading");
+// } else {
+//   console.log("I am not reading");
+// }
+
+const liquid = "water";
+const drink = " water ";
+if (liquid.trim() === drink.trim()) {
+  console.log("life is water");
+} else {
+  console.log("Poluted water");
+}
