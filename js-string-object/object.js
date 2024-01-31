9ecb10eba80790e7a9b1bdaa9ffdeba1edf55133
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-31 11:16:25 +0600

    learned to declaring object

---
+++ js-string-object/object.js
@@ -0,0 +1,15 @@
+const age = 21;
+const schol = "rifle square";
+const isPassed = true;
+let isDeeloper;
+const subjects = ["bangla", "English", "Math"];
+
+//OBJECT
+const bottle = {
+  color: "white",
+  price: 45,
+  nationlity: "BAngladeshi",
+  brand: "Apple",
+  contain: "Water",
+};
+console.log(bottle.color);
