56b2d50350840e15f85b026949f8d4cf3f9f43a8
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-31 10:28:02 +0600

    learned slice and concat

---
+++ js-string-object/concat.js
@@ -0,0 +1,8 @@
+const first = "Arif";
+const last = "Islam";
+const fullName = first + " " + last;
+// console.log(fullName);
+const fullName2 = first.concat(" ").concat(last);
+console.log(fullName2);
+// INCLUDE
+console.log(last.includes("Y"));
---
+++ js-string-object/slice.js
@@ -0,0 +1,14 @@
+const address = "Nilphamari";
+const part = address.slice(3, 6);
+// console.log(part);
+// console.log(address);
+// SUB-STRING IS SIMILAR TO SLICE ==> SUBSTR()
+
+const sentence = "I am a good and hardworking person";
+// console.log(sentence.split(" "));
+const friendsStr = "Arif,Karif,Sarif,Jarif,Marif";
+const friends = friendsStr.split(",");
+// console.log(friends);
+const realFriend = ["Arif", "Karif", "Sarif", "Jarif", "Marif"];
+const fri = realFriend.join("|");
+console.log(fri);
