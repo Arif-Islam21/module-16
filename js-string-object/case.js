9c2d369b3c5bd78ac92c5ec754e5f92d28f6de1a
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-31 09:47:10 +0600

    string definition done

---
+++ js-string-object/case.js
@@ -0,0 +1,5 @@
+// uppercase and lower case matter in programmingn language
+const school = "Kurigram Politechnic Institute";
+console.log(school);
+console.log(school.toLowerCase());
+console.log(school.toUpperCase());
---
+++ js-string-object/string.js
@@ -0,0 +1,14 @@
+const country = "Bangladesh";
+const division = "Rangpour";
+const district = `Nilphamari`;
+const thana = new String("Sadar"); //Special Purpose
+
+// console.log(typeof thana);
+const capital = "dhaka";
+// console.log(capital.length);
+// console.log(capital[3]);
+console.log(capital);
+capital[1] = "c";
+console.log(capital);
+// STRING IS IMMUTABLE
+// TODO WE CAN NOT CHANGE STRING VALUE BY INDEX
