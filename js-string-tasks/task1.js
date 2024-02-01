7613e7d4211dff40ce99da4189cad58b7e36067f
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-01 11:42:50 +0600

    string task 1 done

---
+++ js-string-tasks/task1.js
@@ -0,0 +1,9 @@
+let myself = "I am arif and i am a learner. I want to be a web developer";
+let aCount = 0;
+for (let i = 0; i < myself.length; i++) {
+  //   console.log(myself[i]);
+  if (myself[i] === "a") {
+    aCount += 1;
+  }
+}
+console.log(aCount);
