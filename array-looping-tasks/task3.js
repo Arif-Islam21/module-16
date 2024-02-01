79df6eeea5e9549cc056442ebe89d5fd07f53840
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-01 22:16:37 +0600

    task 3 done

---
+++ array-looping-tasks/task3.js
@@ -0,0 +1,7 @@
+const numbers = ["Tom", "Tim", "Tin", "Tik"];
+let collection = "";
+for (const number of numbers) {
+  //   console.log(number);
+  collection += number;
+}
+console.log(collection);
