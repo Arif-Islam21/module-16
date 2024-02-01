7a4d3021f13f83985658d983c86757c92e5bc6bc
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-01 23:02:33 +0600

    1 task unfinished

--- js-string-tasks/task4.js
+++ js-string-tasks/task4.js
@@ -1,11 +1,14 @@
 let content =
   "I am a programmer , wprking at x company x x . I am studyign at X institude";
-
-const words = content.split(" ");
-let upContent = [];
+const search = "x";
+const replace = "y";
+let words = content.split(" ");
 for (let i = 0; i < words.length; i++) {
-  const value = words[i];
-  // const upWords = value[0].toUpperCase();
-  upContent.push(value[0]);
+  if (words[i] === search) {
+    words[i] = replace;
+  } else if (words[i] === "X") {
+    words[i] = "Y";
+  }
 }
-console.log(upContent);
+let newContent = words.join(" ");
+console.log(newContent);
---
+++ js-string-tasks/task5.js
@@ -0,0 +1,6 @@
+let content =
+  "I am a programmer , wprking at x company x x . I am studyign at X institude";
+let words = content.split(" ");
+for (let i = 0; i < words.length; i++) {
+  if(content[i] === 'x')
+}
