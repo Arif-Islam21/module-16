2b149eea505c2dc6f629708b7cd9f19d1f97b0b5
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-01 10:14:47 +0600

    learned about basic sort

---
+++ js-array-more/sort.js
@@ -0,0 +1,13 @@
+const persons = ["Arif", "Rakib", "SAkib", "NAkib", "Dakib"];
+const sortedPerson = persons.sort();
+console.log(sortedPerson);
+
+const numbers = [4, 7, 2, 9, 1, 8, 2, 5];
+/**
+ * number sorting ==>
+ * Assending
+ * Descending
+ */
+
+const number_asc = numbers.sort();
+console.log(number_asc);
